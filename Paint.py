1a4774fe375516d60ad9cc4d84a8796e5270faf0
leidyvereno <leidy.vereno@correounivalle.edu.co>
2024-05-23 17:01:15 -0500

    se agregan nuevos cambios

--- Paint.py
+++ Paint.py
@@ -8,19 +8,48 @@ class Paint():
         self.ventana.resizable(0,0)
 
         frame1 = Frame(self.ventana, height=100, width=1100, bg="light gray")
-        frame1.grid(row=0, column=0)
+        frame1.grid(row=0, column=0, sticky= NW)
+
+        toolsFrame = Frame(frame1 , height=100 , width=100 , bg="green")
+        toolsFrame.grid(row=0 , column=0)
+
+        pencilButton = Button(toolsFrame , text="Pencil" , width=10)
+        pencilButton.grid(row=0 , column=0)
+
+        eraserButton = Button(toolsFrame , text="Eraser" , width=10)
+        eraserButton.grid(row=1 , column=0)
+
+        toolsLabel = Label(toolsFrame , text="Tools" , width=10)
+        toolsLabel.grid(row=2 , column=0)
 
         frame2 = Frame(self.ventana, height=100, width=1100, bg="light blue")
         frame2.grid(row=1, column=0)
+
         canvas = Canvas(frame2, height=500, width=1100, bg="white")
         canvas.grid(row=0, column=0)
 
-    def paint(event):
-        x = event.x
-        y = event.y
-        canvas.create_oval(x, y, x + 20, y + 20, fill="black")
+        prevpoint =[0,0]
+        currentpoint = [0,0]
+
+        def paint(event):
+            print(event.type)
+            global prevpoint
+            global currentpoint
+            x = event.x
+            y = event.y
+            currentpoint = [x,y]
+            canvas.create_oval(x, y, x + 5, y + 5, fill="black")
+
+            if prevpoint != [0,0]:
+                canvas.create_line(prevpoint[0] , prevpoint[1] , currentpoint[0] , currentpoint[1])
+
+            prevpoint = currentpoint
+
+            if event.type == "5" :
+                prevpoint = [0,0]
 
         canvas.bind("<B1-Motion>", paint)
+        canvas.bind("<ButtonRelease-1>" , paint)
 
         self.ventana.mainloop()
 
