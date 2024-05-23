2c626e5165975390d8ea9ed031166a62809c4982
AnaaS27 <ana.chocue@correounivalle.edu.co>
2024-05-23 17:41:10 -0500

    se realizan nuevos cambios

--- Paint.py
+++ Paint.py
@@ -6,47 +6,56 @@ class Paint():
         self.ventana.title("Paint")
         self.ventana.geometry("1100x600")
         self.ventana.resizable(0,0)
-
+        
         frame1 = Frame(self.ventana, height=100, width=1100, bg="light gray")
         frame1.grid(row=0, column=0, sticky= NW)
 
         toolsFrame = Frame(frame1 , height=100 , width=100 , bg="green")
         toolsFrame.grid(row=0 , column=0)
 
-        pencilButton = Button(toolsFrame , text="Pencil" , width=10)
+        def usePencil():
+            stroke_color.set("black")
+
+        def useEraser():
+            stroke_color.set("white")
+            canvas["cursor"] = DOTBOX
+
+        pencilButton = Button(toolsFrame , text="Pencil" , width=10, command=usePencil)
         pencilButton.grid(row=0 , column=0)
 
-        eraserButton = Button(toolsFrame , text="Eraser" , width=10)
+        eraserButton = Button(toolsFrame , text="Eraser" , width=10, command=useEraser)
         eraserButton.grid(row=1 , column=0)
 
         toolsLabel = Label(toolsFrame , text="Tools" , width=10)
-        toolsLabel.grid(row=2 , column=0)
+        toolsLabel.grid(row=3 , column=0)
 
-        frame2 = Frame(self.ventana, height=100, width=1100, bg="light blue")
+        frame2 = Frame(self.ventana, height=500, width=1100, bg="light blue")
         frame2.grid(row=1, column=0)
 
         canvas = Canvas(frame2, height=500, width=1100, bg="white")
         canvas.grid(row=0, column=0)
 
-        prevpoint =[0,0]
-        currentpoint = [0,0]
+        stroke_color = StringVar()
+        stroke_color.set("black")
+
+        prevPoint = [0,0]
+        currentPoint = [0,0]
 
         def paint(event):
-            print(event.type)
-            global prevpoint
-            global currentpoint
+            global prevPoint
+            global currentPoint
             x = event.x
             y = event.y
-            currentpoint = [x,y]
-            canvas.create_oval(x, y, x + 5, y + 5, fill="black")
+            currentPoint = [x,y]
+            #canvas.create_oval(x, y, x + 5, y + 5, fill="black")
 
-            if prevpoint != [0,0]:
-                canvas.create_line(prevpoint[0] , prevpoint[1] , currentpoint[0] , currentpoint[1])
+            if prevPoint != [0,0] :
+                canvas.create_line(prevPoint[0] , prevPoint[1] , currentPoint[0] , currentPoint[1], fill = stroke_color.get())
 
-            prevpoint = currentpoint
+            prevPoint = currentPoint
 
             if event.type == "5" :
-                prevpoint = [0,0]
+                prevPoint = [0,0]
 
         canvas.bind("<B1-Motion>", paint)
         canvas.bind("<ButtonRelease-1>" , paint)
