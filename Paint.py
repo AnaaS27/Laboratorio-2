5ba9dfa9ae936471ce228016067fae2a15107e20
Danna077 <danna.florez@correounivalle.edu.co>
2024-05-23 16:03:39 -0500

    Se agg la funcion paint

--- Paint.py
+++ Paint.py
@@ -12,7 +12,15 @@ class Paint():
 
         frame2 = Frame(self.ventana, height=100, width=1100, bg="light blue")
         frame2.grid(row=1, column=0)
-        
+        canvas = Canvas(frame2, height=500, width=1100, bg="white")
+        canvas.grid(row=0, column=0)
+
+    def paint(event):
+        x = event.x
+        y = event.y
+        canvas.create_oval(x, y, x + 20, y + 20, fill="black")
+
+        canvas.bind("<B1-Motion>", paint)
 
         self.ventana.mainloop()
 
