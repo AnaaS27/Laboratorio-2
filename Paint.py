8b13801f88e0feef4115d9b15f786cab3619cb12
leidyvereno <leidy.vereno@correounivalle.edu.co>
2024-05-23 22:05:58 -0500

    se agrega la funcion para seleccionar un color

--- Paint.py
+++ Paint.py
@@ -57,10 +57,27 @@ class Paint():
 
         def selectColor():
             selectedColor = colorchooser.askcolor("blue", title="Select Color")
+            if selectedColor[1] == None :
+             stroke_color.set("black")
+            else:
+             stroke_color.set(selectedColor[1])
             print(selectedColor)
 
         colorBoxButton = Button(colorBoxFrame, text="Select Color", width=10, command=selectColor)
         colorBoxButton.grid(row=0, column=0)
+
+        colorsFrame = Frame(frame1, height=100, width=100, relief=SUNKEN, borderwidth=3,)
+        colorsFrame.grid(row = 0 , column=3)
+
+        redButton = Button(colorsFrame , text="Red" , bg="Red" , width=10 , command=lambda: stroke_color.set("red"))
+        redButton.grid(row=0, column=0)
+
+        greenButton = Button(colorsFrame , text="Green" , bg="Green" , width=10 , command=lambda: stroke_color.set("Green"))
+        greenButton.grid(row=1, column=0)
+
+        blueButton = Button(colorsFrame , text="Blue" , bg="Blue" , width=10 , command=lambda: stroke_color.set("Blue"))
+        blueButton.grid(row=2 , column=0) 
+
 
         frame2 = Frame(self.ventana, height=500, width=1100, bg="light blue")
         frame2.grid(row=1, column=0)
