6744c89b1368232c19e2b59446e1bfeee106ecb6
AnaaS27 <ana.chocue@correounivalle.edu.co>
2024-05-25 17:37:20 -0500

    Se agrega la funcion para pintar con el click derecho, y se realizan otros cambios

--- Paint.py
+++ Paint.py
@@ -51,16 +51,21 @@ class Paint():
         def paint(self, event, prevPoint, currentPoint):
             x = event.x
             y = event.y
-            self.currentPoint = [x,y]
+            currentPoint = [x,y]
             #canvas.create_oval(x, y, x + 5, y + 5, fill="black")
 
             if prevPoint != [0,0] :
-                canvas.create_polygon(prevPoint[0] , prevPoint[1] , self.currentPoint[0] , self.currentPoint[1], fill = stroke_color.get(), outline=stroke_color.get(), width=stroke_size.get())
+                canvas.create_polygon(prevPoint[0] , prevPoint[1] , currentPoint[0] , currentPoint[1], fill = stroke_color.get(), outline=stroke_color.get(), width=stroke_size.get())
 
-            self.prevPoint = self.currentPoint
+            self.prevPoint = currentPoint
 
             if event.type == "5" :
                 self.prevPoint = [0,0]
+        
+        def paintRight(event):
+            x = event.x
+            y = event.y
+            canvas.create_arc(x,y,x+stroke_size.get(), y+stroke_size.get(), fill = stroke_color.get(), outline=stroke_color.get(), width=stroke_size.get())
 
         def saveImage():
             try:
@@ -82,6 +87,15 @@ class Paint():
         def createNew():
             if messagebox.askyesno("Paint" , "¿Quieres guardar los cambios antes de borrar todo?"):
                 clear()
+        
+        def help():
+            messagebox.showinfo("Ayuda", "1. Click en el Boton Seleccionar Color para elegir un color especifico\n2. Click en el Boton Limpiar para limpiar el Lienzo")
+
+        def settings():
+            messagebox.showwarning("Configuracion", "No disponible" )
+        
+        def about():
+            messagebox.showinfo("Acerca de", "Aplicacion de Pintura para estimular la creatividad de las personas")
             
         frame1 = Frame(self.ventana, height=100, width=1100)
         frame1.grid(row=0, column=0, sticky= NW)
@@ -144,6 +158,18 @@ class Paint():
         saveImageButton = Button(saveImageFrame , text="Save" , bg="white" , width=10 , command=saveImage)
         saveImageButton.grid(row=0, column=0)
 
+        helpSettingFrame = Frame(frame1, height=100, width=100, relief=SUNKEN, borderwidth=3)
+        helpSettingFrame.grid(row=0, column=5)
+
+        helpButton = Button(helpSettingFrame, text="Help", bg="white", width=10, command=help)
+        helpButton.grid(row=0, column=0)
+
+        settingButton = Button(helpSettingFrame, text="Settings", bg="white", width=10, command=settings)
+        settingButton.grid(row=1, column=0)
+
+        aboutButton = Button(helpSettingFrame, text="About", bg="white", width=10, command=about)
+        aboutButton.grid(row=2, column=0)
+
         frame2 = Frame(self.ventana, height=500, width=1100, bg="light blue")
         frame2.grid(row=1, column=0)
 
@@ -151,6 +177,7 @@ class Paint():
         canvas.grid(row=0, column=0)
         canvas.bind("<B1-Motion>", lambda event: paint(self, event, self.prevPoint, self.currentPoint))
         canvas.bind("<ButtonRelease-1>" , lambda event: paint(self, event, self.prevPoint, self.currentPoint))
+        canvas.bind("<B3-Motion>", paintRight)
 
         self.ventana.mainloop()
 
